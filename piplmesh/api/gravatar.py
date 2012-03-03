dd789235b07aa502a16c5cae99b67e5c6ff2adc8
Domen <domen@cebulj.net>
2012-03-03 13:54:20 +0100

    Fixed & changed - get gravatar only with URL

--- piplmesh/api/gravatar.py
+++ piplmesh/api/gravatar.py
@@ -2,17 +2,9 @@
 import urllib, hashlib
 
 # avatar function which return URL to avatar picture
-def gravatarURL(email, size = 40, default = STATIC_URL+piplmesh/images/+"avatar.png"):
+def gravatarURL(email, size = 40):
 	# construct the url
 	gravatarUrl = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-	gravatarUrl += urllib.urlencode({'d':default, 's':str(size)})
-	
+	gravatarUrl += urllib.urlencode({'s':str(size)})
+
 	return gravatarUrl
-	
-# avatar function which return IMG tag to avatar picture
-def gravatarImgTag(email, size = 40, default = "avatar.png"):
-	url = avatarUrl(email = email, size = size, defualt = default)
-	imgTagUrl = "<img src=" + url + "\" border=\"0\" />";
-	
-	return imgTagUrl
-	
