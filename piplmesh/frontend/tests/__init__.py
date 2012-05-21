75147732cd04ebe9758d5e8292664486067785fd
David Mozina <cardawid@gmail.com>
2012-05-22 00:09:23 +0200

    issue#107 team3

---
+++ piplmesh/frontend/tests/__init__.py
@@ -0,0 +1,13 @@
+#from tests import *
+
+from __future__ import absolute_import
+
+from django.utils import unittest
+
+from . import tests
+
+def suite():
+    return unittest.TestSuite((
+        unittest.TestLoader().loadTestsFromTestCase(tests.BasicTest),
+        #unittest.TestLoader().loadTestsFromTestCase(tests.UserTest),
+    ))
--- piplmesh/frontend/tests/tests.py
+++ piplmesh/frontend/tests/tests.py
@@ -1,27 +1,40 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = "piplmesh.settings"
-
-from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 from django.utils import unittest
+from django.db import models
+from piplmesh import test_runner
 
-class Testing(TestCase):
+class BasicTest(unittest.TestCase):
 	def setUp(self):
-        self.client = Client()
-                        
-    def test__login(self):
-		c = Client()
-		c.login(username='test', password='testtest')
-		self.assertEqual(response.status_code, 200)
-	
-	def test_logout(self):
-		c = Client()
-		c.logout()
+		self.client = Client()
 		
-	def user_loged_on(self):
+	#Can NOT create user											
+	def test_create_user(self):
+		test_user = User.objects.create(
+					username		= 	'test',
+					first_name		= 	'test_firstname',
+					last_name		= 	'test_lastname',
+					email			= 	'test@test.si',
+					gender			= 	'male',
+					)
+		test_user.set_password('testtest')
+		test_user.save()
+		
+	def test__login(self):
+		self.client.login(username='test', password='testtest')
+		print 'Logged on.'
+
+			                               
+	def test_user_loged_on(self):
 		response = self.client.get('/user/test/')
 		self.assertEqual(response.status_code, 200)
+		print response.status_code
+
+	def test_logout(self):
+		self.client.logout()
+		print '\nLogged out.'
 		
- 
-  
+	def test_registration(self):
+		response = self.client.get('/register/')
+		self.assertEqual(response.status_code, 200)
+		print response.status_code
