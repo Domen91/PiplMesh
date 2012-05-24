b4cfdd4a3a7892f606a0971c324a263d4d5a3225
David Mozina <cardawid@gmail.com>
2012-05-25 00:10:57 +0200

    Registration test fixed

--- piplmesh/frontend/tests/tests.py
+++ piplmesh/frontend/tests/tests.py
@@ -1,14 +1,13 @@
 from django.test.client import Client
-from django.contrib.auth.models import User
+from piplmesh.account.models import User
 from django.utils import unittest
 from django.db import models
 from piplmesh import test_runner
 
-class BasicTest(unittest.TestCase):
+class BasicTest(test_runner.MongoEngineTestCase):
 	def setUp(self):
 		self.client = Client()
-		
-	#Can NOT create user											
+													
 	def test_create_user(self):
 		test_user = User.objects.create(
 					username		= 	'test',
@@ -19,17 +18,12 @@ class BasicTest(unittest.TestCase):
 					)
 		test_user.set_password('testtest')
 		test_user.save()
+		print 'User ' + test_user.get_full_name() + ' created.'
 		
 	def test__login(self):
 		self.client.login(username='test', password='testtest')
 		print 'Logged on.'
-
 			                               
-	def test_user_loged_on(self):
-		response = self.client.get('/user/test/')
-		self.assertEqual(response.status_code, 200)
-		print response.status_code
-
 	def test_logout(self):
 		self.client.logout()
 		print '\nLogged out.'
