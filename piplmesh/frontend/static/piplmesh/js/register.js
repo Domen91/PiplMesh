9f2c2506a9ae5f5bf170b1ff479a93877803dcb8
Domen91 <domen@cebulj.net>
2012-06-07 23:22:14 +0200

    Add email JS checker

--- piplmesh/frontend/static/piplmesh/js/register.js
+++ piplmesh/frontend/static/piplmesh/js/register.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('#id_password2').keyup(checkPassword);
+    $('#id_email').keyup(checkEmail);
 });
 
 function checkPassword() {
@@ -14,5 +15,19 @@ function checkPassword() {
     {
         $(pass1).css('background-color','#ff6666');
         $(pass2).css('background-color','#ff6666');
+    }
+}
+
+function checkEmail() {
+    var email = "#id_email";
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+    var emailValue = $(email).val();
+    if (!emailReg.test(emailValue)) {
+        $(email).css('background-color','#ff6666');
+    }
+    else
+    {
+        $(email).css('background-color','#66cc66');
     }
 }
